d11e70e26ec867d0f334103fe88de8d210779b76
Andy Bu <andybuit@gmail.com>
2019-09-29 10:57:49 -0400

    Applo
    Apollo integration

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,69 @@
 import * as express from "express";
+import { ApolloServer } from "apollo-server";
+import * as path from "path";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/crmRoutes";
 import * as mongoose from "mongoose";
+import { buildSchema } from "type-graphql";
+import { RecipeResolver } from "./resolvers/recipe-resolver";
+import { RateResolver } from "./resolvers/rate-resolver";
+import { User } from "./entities/user";
+import { TypegooseMiddleware } from "./typegoose-middleware";
+import { seedDatabase } from "./helpers";
+import { ObjectId } from "mongodb";
+import { ObjectIdScalar } from "./object-id.scalar";
+
+export interface Context {
+  user: User;
+}
 
 class App {
+  public app: ApolloServer;
+  public routePrv: Routes = new Routes();
+  public mongoUrl: string =
+    "mongodb+srv://suianbu:Bsa770111@cluster0-m5knr.mongodb.net/wtp?retryWrites=true&w=majority";
 
-    public app: express.Application;
-    public routePrv: Routes = new Routes();
-    public mongoUrl: string = 'mongodb://localhost/webpricetrack';  
+  constructor() {
+    this.apolloServerSetup();
+    this.config();
+    this.routePrv.routes(this.app);
+    this.mongoSetup();
+  }
 
-    constructor() {
-        this.app = express();
-        this.config();        
-        this.routePrv.routes(this.app);     
-        this.mongoSetup();
-    }
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
 
-    private config(): void{
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-    }
+  private mongoSetup(): void {
+    mongoose
+      .connect(this.mongoUrl, { useNewUrlParser: true })
+      .then(() => {
+        return console.info(`Successfully connected to ${this.mongoUrl}`);
+      })
+      .catch(error => {
+        console.error("Error connecting to database: ", error);
+        return process.exit(1);
+      });
+  }
 
-    private mongoSetup(): void{
-        mongoose.Promise = global.Promise;
-        mongoose.connect(this.mongoUrl);    
-    }
+  private async apolloServerSetup() {
+    const { defaultUser } = await seedDatabase();
+    const schema = await buildSchema({
+      resolvers: [RecipeResolver, RateResolver],
+      emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+      // use document converting middleware
+      globalMiddlewares: [TypegooseMiddleware],
+      // use ObjectId scalar mapping
+      scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }]
+    });
+
+    // create mocked context
+    const context: Context = { user: defaultUser };
+
+    // Create GraphQL server
+    this.app = new ApolloServer({ schema, context });
+  }
 }
 
 export default new App().app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
+import "reflect-metadata";
 import app from "./app";
 const PORT = 4000;
 
 app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
-})
+  console.log("Express server listening on port " + PORT);
+});
