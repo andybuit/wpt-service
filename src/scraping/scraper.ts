fbcbbc7c5210db2f619504e98d630bb93a25ed0a
Andy Bu <andybuit@gmail.com>
2019-10-02 17:03:25 -0400

    test

--- src/scraping/scraper.ts
+++ src/scraping/scraper.ts
@@ -1,15 +1,32 @@
-import * as puppeteer from "puppeteer";
+import * as puppeteer from "puppeteer-extra";
 import * as $ from "cheerio";
+import * as fs from "fs";
+import * as scrollPageToBottom from "puppeteer-autoscroll-down";
+import * as pluginStealth from "puppeteer-extra-plugin-stealth";
 
-const url = "https://www.reddit.com";
+const url =
+  "https://www.costco.ca/LG-2-piece-Laundry-Suite-5.2-cu.ft-Front-Load-Washer-with-SmartThinQ%c2%ae-Technology-and-7.4-cu.ft-Dryer-with-Smart-Diagnosis%e2%84%a2.product.100427940.html";
 
 export async function scraping() {
-  console.log(111111111111)
+  console.log(111111111111);
+  puppeteer.use(pluginStealth());
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.setViewport({ width: 800, height: 600 });
   await page.goto(url);
+  await page.waitFor(2000);
   const html = await page.content();
-  $("h2", html).each(function() {
+
+  fs.writeFile("c:\\temp\\web.html", html, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+  });
+  $(
+    "div:nth-child(4)>div:nth-child(2)>div>div>div:nth-child(2)>div>span:nth-child(1)",
+    html
+  ).each(function() {
     console.log($(this).text());
   });
 
