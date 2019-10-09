c08082d5a660a2e2398a5118ea4cbdc890074c3c
Andy Bu <suianbu@gmail.com>
2019-10-08 21:01:06 -0400

    fix bug

--- src/resolvers/types/rule.input.ts
+++ src/resolvers/types/rule.input.ts
@@ -1,6 +1,6 @@
 import { Field, InputType } from "type-graphql";
 import { ValueType } from "../../enums/value-type";
-import { Rule } from "entities/rule";
+import { Rule } from "../../entities/rule";
 
 @InputType()
 export class RuleInput implements Partial<Rule>{
